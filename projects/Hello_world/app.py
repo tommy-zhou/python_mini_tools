ee9ea45cddffcc05718011f6314efc629782e53d
周军 <zhoujun_723@hotmail.com>
2022-05-18 22:48:30 +0800

    Hello world code submit!

--- projects/Hello_world/app.py
+++ projects/Hello_world/app.py
@@ -3,13 +3,25 @@ import time
 if __name__ == "__main__":
     #print函数讲解
     print("Hello python world!")
+
     a = "Hello "
     b = 1
     print(a,b)
+
     print("www","github","com",sep=".")
-    print("---RUNOOB EXAMPLE ： Loading 效果---")
-    print("Loading",end = "")
+
+    print("--- EXAMPLE ： Loading 效果---")
+    print("Loading",end = "",flush = False)
     for i in range(20):
-        print(".",end = '',flush = True)
+        print(".",end = '',flush = False)
         time.sleep(0.5)
+
+    file = open("hello_world.txt","w")
+    print("--- EXAMPLE ： Loading 效果---")
+    print("Loading",end = "",file=file,flush = False)
+    for i in range(50):
+        print(".",end = '',file=file,flush = False)
+        time.sleep(0.5)
+    file.close()
+
     #python的六个基本数据类型讲解
