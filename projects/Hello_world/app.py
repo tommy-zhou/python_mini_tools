adcea4934d47893a58cc397de943f3b9f463bc24
周军 <zhoujun_723@hotmail.com>
2022-05-18 22:35:17 +0800

    Hello world code submit!

--- projects/Hello_world/app.py
+++ projects/Hello_world/app.py
@@ -1,4 +1,15 @@
+import time
 
 if __name__ == "__main__":
+    #print函数讲解
     print("Hello python world!")
-    print("\a")
+    a = "Hello "
+    b = 1
+    print(a,b)
+    print("www","github","com",sep=".")
+    print("---RUNOOB EXAMPLE ： Loading 效果---")
+    print("Loading",end = "")
+    for i in range(20):
+        print(".",end = '',flush = True)
+        time.sleep(0.5)
+    #python的六个基本数据类型讲解
